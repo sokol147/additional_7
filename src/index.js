b3aef5e5d77c320712f53be2e11e2be2af3993b3
Sokolov V <sokolov.stb@gmail.com>
2018-10-11 12:43:55 +0300

    refactor: remove extra functions

--- src/index.js
+++ src/index.js
@@ -1,64 +1,33 @@
 module.exports = function solveSudoku(matrix) {
-  function solve(matrix, row, col){
-    let cell = findEmptyCell(matrix, row, col);
-    row = cell[0];
-    col = cell[1];
+  const newMatrix = matrix;
 
-    if(row == -1) return true;
-
-    for(let num = 1; num <= 9; num++){
-      if(markSolved(matrix, row, col, num)){
-        matrix[row][col] = num;
-
-        if(solve(matrix, row, col)) return true;
-
-        matrix[row][col] = 0;
-      }
-    }
-    return false;
-  }
-
-  function findEmptyCell(matrix, row, col){
-    for(; row < 9; col = 0, row++){
-      for(; col < 9; col++){
-        if(matrix[row][col] == 0) return [row, col];
-      }
-    }
-    return [-1, -1];
-  }
-
-  function markSolved(matrix, row, col, num){
-    return checkRow(matrix, row, num) &&
-           checkCol(matrix, col, num) &&
-           checkSqure(matrix, row, col, num);
-  }
-
-  function checkRow(matrix, row, num){
+  for(let row = 0; row < 9; row++){
     for(let col = 0; col < 9; col++){
-      if(matrix[row][col] == num) return false;
-    }
-    return true;
-  }
-
-  function checkCol(matrix, col, num){
-    for(let row = 0; row < 9; row++){
-      if(matrix[row][col] == num) return false;
-    }
-    return true;
-  }
-
-  function checkSqure(matrix, row, col, num){
-    row = Math.floor(row / 3) * 3;
-    col = Math.floor(col / 3) * 3;
-
-    for(let i = 0; i < 3; i++){
-      for(let j = 0; j < 3; j++){
-        if(matrix[row + i][col + j] == num) return false;
+      if(matrix[row][col] === 0){
+        const suggestions = count(row, col, newMatrix);
+        
+        for(let suggestion of suggestions){
+          newMatrix[row][col] = suggestion;
+          solveSudoku(newMatrix);
+        }
       }
     }
-    return true;
   }
+  return newMatrix;
+}
 
-  solve(matrix, 0, 0);
-  return matrix;
+function count(row, col, matrix){
+  const result = [];
+
+  row = help(row) * 3;
+  col = help(col) * 3;
+
+  for(let i = 0; i < 9; i++){
+    result.push([matrix[row][i], matrix[i][col], matrix[row + i % 3][col + help(i)]]);
+  }
+  return result;
+}
+
+function help(num){
+  return Math.floor(num / 3);
 }
